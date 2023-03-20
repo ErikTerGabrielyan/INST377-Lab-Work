eb7aaa321f2737e417395249c2599a0a58fae937
Erik Ter-Gabrielyan <eriktergabrielyan@yahoo.com>
2023-03-20 00:29:46 -0400

    Recommitting Lab 5 - forgot the first time

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,18 +5,16 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
-  /*
-    Using the .filter array method, 
-    return a list that is filtered by comparing the item name in lower case
-    to the query in lower case
-
-    Ask the TAs if you need help with this
-  */
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  // Add a querySelector that targets your filter button here
+  const filterButton = document.querySelector('.filter_button');
 
   let currentList = []; // this is "scoped" to the main event function
   
@@ -55,19 +53,15 @@ async function mainEvent() { // the async keyword means we can make API requests
   });
 
 
-  /*
-    Now that you HAVE a list loaded, write an event listener set to your filter button
-    it should use the 'new FormData(target-form)' method to read the contents of your main form
-    and the Object.fromEntries() method to convert that data to an object we can work with
-
-    When you have the contents of the form, use the placeholder at line 7
-    to write a list filter
-
-    Fire it here and filter for the word "pizza"
-    you should get approximately 46 results
-  */
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton');
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+    console.log(newList);
+  })
 }
-
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
   The use of the async keyword means we can "await" events before continuing in our scripts
